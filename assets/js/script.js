01aff141560a2f46602806bf96014b9b8b271e7b
hyper-vlados <hypervlados@gmail.com>
2020-11-24 18:21:41 +0200

    Folders

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ const reviewStarsList = document.querySelectorAll('.review__star');
 const reviewStars = Array().slice.call(reviewStarsList);
 
 reviewStars.forEach(star =>
-    star.addEventListener('click', () =>
-        star.parentNode.dataset.currentStars = star.dataset.starValue
-    )
+    star.addEventListener('click', () => {
+            const {starValue} = star.dataset;
+            star.parentNode.dataset.currentStars = starValue;
+            alert(starValue);
+        })
 );
